3f11875d62cbfcceea6fa3e2c8e97cda1d4549b9
Matt Weeden <matt.weeden@myhealthaccess.net>
2018-12-03 22:08:54 -0600

    Add notes and commands dictionary

--- game.py
+++ game.py
@@ -2,14 +2,31 @@
 import gen_methods as gm
 # import an_methods as am
 import walk_texts as ws
-
 import sys
+
+
 
 WIDTH = 120
 HEIGHT = 30
 TIMEOUT = 100
 
 WT_SET = list(ws.walk_text)
+
+COMMAND_D = {
+        "quit": ["quit", "q", "exit"],
+        "status": ["status", "s"],
+        "walk": ["walk", "w"],
+        "help": ["help", "h"]
+}
+
+
+HELP_MESSAGE = """
+  Commands:
+    w   walk
+    s   status
+    h   help
+    q   quit"""
+
 
 def main():
 
@@ -32,16 +49,19 @@ def main():
 def play(p1, debug=False):
     while (True):
         command = raw_input("\nEnter a command: ")
-        if command in ['q', 'quit', 'exit']:
+        if command in COMMAND_D['quit']:
             break
 
-        elif command in ['w', 'walk']:
+        elif command in COMMAND_D["help"]:
+            print HELP_MESSAGE
+
+        elif command in COMMAND_D['walk']:
             if p1.r.randint(0, 9) == 0:
                 wt = p1.r.choice(WT_SET)
                 WT_SET.remove(wt)
             p1.update('walk')
 
-        elif command in ['s', 'status']:
+        elif command in COMMAND_D['status']:
             p1.prettyPrint()
 
 if __name__ == "__main__":
