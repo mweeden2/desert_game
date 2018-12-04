aaf561d11687455a23982771315b15a9bf5b1a86
Matt Weeden <matt.weeden@myhealthaccess.net>
2018-12-03 22:09:09 -0600

    Add file with walking lines

--- walk_texts.py
+++ walk_texts.py
@@ -2,8 +2,9 @@
 # 10/15/18
 
 walk_text = [
-    "You've walked now for three hours, hoping to find something on the flat flat horizon.",
-    "An small cleft appears where you would've stepped, but you avoid it. \"I have no idea how that got there.\"",
-    "The sky is especially blue today, kind of like a baby's room's walls, but where the color and light come from separate and larger places.",
-    "You can't stop thinking about what happened earlier today, like an open wound that you know will heal by next week.'"
+    "You've walked now for three hours, hoping to find something on the flat horizon...Now that you see it, it's strikingly straight. _I's amazing something out here could be so straight.",
+    "A short ridge appears in the sand. You step aside with a start. The rock is fairly straight, but disappears into the coming sand dune. It's like the first stair step to a large courthouse without a second.",
+    "The sky is especially blue today, kind of like a baby's room's walls, but where the color and light come from much more separate places.",
+    "You can't stop thinking about what happened earlier today, like an open wound that you know will heal by next week.",
+    "You try to walk lightly so as not to sink into the sand. Your feet would love something more solid.",
 ]
